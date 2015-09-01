883823b13b06b1dad3248bd3c55853c6539ba25f
Jannick Fahlbusch <git@jf-projects.de>
2015-09-01 17:25:01 +0200

    The $toNumber-param is optional

--- qr.php
+++ qr.php
@@ -5,6 +5,6 @@ class qr extends Script
 {
     public function run()
     {
-        return $this->send($this->message->number, 'https://chart.googleapis.com/chart?chs=547x547&cht=qr&chl=' . urlencode($this->matches[1]), 'image');
+        return $this->send('https://chart.googleapis.com/chart?chs=547x547&cht=qr&chl=' . urlencode($this->matches[1]), 'image');
     }
 }
