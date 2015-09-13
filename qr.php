57c6de62ae64f191a5d50778ca06d4f4c73aadea
Jannick Fahlbusch <git@jf-projects.de>
2015-09-13 22:42:09 +0200

    Complete the information for the help-plugin

--- qr.php
+++ qr.php
@@ -1,6 +1,9 @@
 <?php
 class qr extends Script
 {
+    protected $helpMessage = "'qr STRING'\n";
+    protected $description = 'Returns a QR-code representing the given string';
+
     public function run()
     {
         return $this->send('https://chart.googleapis.com/chart?chs=547x547&cht=qr&chl=' . urlencode($this->matches[1]), 'image');
